c154f22200b39e7924f8fcbce8dde5044a3f2391
Nate Z <ntzeitlin@gmail.com>
2025-03-17 14:11:41 -0500

    Rock list now properly displays data from server

--- src/components/RockList.jsx
+++ src/components/RockList.jsx
@@ -1,23 +1,32 @@
-import { useEffect } from "react"
+import { useEffect } from "react";
 
 export const RockList = ({ rocks, fetchRocks }) => {
     useEffect(() => {
-    }, [])
+        fetchRocks();
+    }, []);
 
     const displayRocks = () => {
         if (rocks && rocks.length) {
-            return rocks.map(rock => <div key={`key-${rock.id}`} className="border p-5 border-solid hover:bg-fuchsia-500 hover:text-violet-50 rounded-md border-violet-900 mt-5 bg-slate-50">
-                {rock.name} ({rock.type.label})
-            </div>)
+            return rocks.map((rock) => (
+                <div
+                    key={`key-${rock.id}`}
+                    className="border p-5 border-solid hover:bg-fuchsia-500 hover:text-violet-50 rounded-md border-violet-900 mt-5 bg-slate-50"
+                >
+                    {rock.name} ({rock.type.label}) weighs {rock.weight} kg{" "}
+                    <br />
+                    In the collection of {rock.user.first_name}{" "}
+                    {rock.user.last_name}
+                </div>
+            ));
         }
 
-        return <h3>Loading Rocks...</h3>
-    }
+        return <h3>Loading Rocks...</h3>;
+    };
 
     return (
         <>
             <h1 className="text-3xl">Rock List</h1>
             {displayRocks()}
         </>
-    )
-}
+    );
+};
